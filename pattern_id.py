9a0f37751aa064ec850d441b05a4eeb6b0142475
Adithya Rajesh Pillai <82641329+adithya16pillai@users.noreply.github.com>
2024-09-02 20:45:20 +0530

    Add files via upload

---
+++ pattern_id.py
@@ -0,0 +1,10 @@
+s = str(input()) #Converts value to string
+l = str(input())
+
+x = s.find(l) #Finds the first occurance of the value/string
+print(x)
+y = len(l) #Used to calculate the length 
+range = x + y #Calculates the 
+
+print(range) #Prints the range
+print(x , 'to' , range) #Prints the final output
