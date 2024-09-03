ff467e11ec77e04bb78a7c1052e6e1f2f3084b10
Adithya Rajesh Pillai <82641329+adithya16pillai@users.noreply.github.com>
2024-09-03 19:52:05 +0530

    Add files via upload

---
+++ oddeven.py
@@ -0,0 +1,7 @@
+#Python code to check whether a given number is odd or even
+num=int(input("Enter a number: "))
+
+if(num%2)==0:
+	print("{0} is Even".format(num))
+else:
+	print("{0} is Odd".format(num))
